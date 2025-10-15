e4390d26bf6071e03035e68423e0a124b45ebbec
James Heiniger <james.heiniger911@gmail.com>
2025-10-15 12:34:52 +1030

    commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+tabBtn.onclick = function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+};
+
+function render(arr) {
+  let listItems = "";
+  arr.forEach(lead => {
+    listItems += `<li><a href="#" target="blank">${lead}</a></li>`;
+  });
+  ulEl.innerHTML = listItems;
+}
+
+inputBtn.onclick = function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+};
+
+deleteBtn.onclick = function () {
+  localStorage.clear();
+  myLeads = [];
+  ulEl.innerHTML = "";
+  render(myLeads);
+};
