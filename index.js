0ee9a6d6e35a2275146566326fa1fd33875f8a1a
James Heiniger <james.heiniger911@gmail.com>
2025-10-23 12:35:13 +1030

    Rendering items using onvalue

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-let myLeads = [];
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+
+const firebaseConfig = {
+  databaseURL:
+    "",
+  // databaseURL: process.env.DATABASE_URL
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const referenceInDB = ref(database, "leads");
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-const tabBtn = document.getElementById("tab-btn");
-
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
-}
-
-tabBtn.onclick = function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url);
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
-  });
-};
 
 function render(arr) {
   let listItems = "";
@@ -28,15 +30,14 @@ function render(arr) {
 }
 
 inputBtn.onclick = function () {
-  myLeads.push(inputEl.value);
+  push(referenceInDB, inputEl.value);
   inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  render(myLeads);
 };
 
-deleteBtn.onclick = function () {
-  localStorage.clear();
-  myLeads = [];
-  ulEl.innerHTML = "";
-  render(myLeads);
-};
+deleteBtn.onclick = function () {};
+
+onValue(referenceInDB, function (snapshot) {
+ const snapshotValues =  snapshot.val()
+ const leads = Object.values(snapshotValues)
+ render(leads)
+});
